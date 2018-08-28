7076749e15e42442cf913fb358e7f9bd00f25578
Cota <alberto.cota.aceves@hp.com>
2018-08-28 17:31:13 -0500

    GUI almost finished

--- app/src/main/java/com/iteso/sesion5/ActivityMain.java
+++ app/src/main/java/com/iteso/sesion5/ActivityMain.java
@@ -2,12 +2,24 @@ package com.iteso.sesion5;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class ActivityMain extends AppCompatActivity {
-
+    Spinner spinner = (Spinner) findViewById(R.id.main_spinner);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.grades, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.activity_menu,menu);
+        return true;
     }
 }
