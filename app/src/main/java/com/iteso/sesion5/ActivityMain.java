d29226fbfc600dfa70d13056daf92ec57e6ec35c
AlbertoCota93 <is694734@iteso.mx>
2018-09-01 12:14:03 -0500

    Failed to create Dialogue, missing Student Class and Toast Building

--- app/src/main/java/com/iteso/sesion5/ActivityMain.java
+++ app/src/main/java/com/iteso/sesion5/ActivityMain.java
@@ -1,5 +1,8 @@
 package com.iteso.sesion5;
 
+import android.content.DialogInterface;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,6 +11,7 @@ import android.view.View;
 import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Spinner;
@@ -18,15 +22,19 @@ public class ActivityMain extends AppCompatActivity {
     Spinner spinner;
     EditText nombre;
     EditText telefono;
-    RadioButton hombre;
-    RadioButton mujer;
     AutoCompleteTextView libros;
     String genere;
+    String sport;
+    Button clean;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nombre = findViewById(R.id.activity_name);
+        telefono = findViewById(R.id.activity_phone);
+        clean = findViewById(R.id.activity_clean);
 
         spinner = findViewById(R.id.main_spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
@@ -39,6 +47,26 @@ public class ActivityMain extends AppCompatActivity {
         ArrayAdapter<String> stringAdapter = new ArrayAdapter<String>
                 (this, android.R.layout.simple_list_item_1, books);
         libros.setAdapter(stringAdapter);
+
+        clean.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(ActivityMain.this);
+                builder.setMessage("Deseas Limpiar?").setPositiveButton("Limpiar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                }).setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                }
+        }
+
+        );
 
 
     }
@@ -71,5 +99,22 @@ public class ActivityMain extends AppCompatActivity {
                 default:
                     genere = "";
         }
+    }
+    public void onCheckboxClicked (View view){
+        boolean check = ((RadioButton)view).isChecked();
+
+        switch (view.getId()){
+            case R.id.activity_checkbox:
+                if(check)
+                    sport = "Si";
+                else
+                    sport = "No";
+            default:
+                sport = "No";
+        }
+    }
+
+    public void confirmCleanAction(){
+      //  DialogFragment dialogFragment = new ConfirmCleanAction();
     }
 }
